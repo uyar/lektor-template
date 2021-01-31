65ef875f4bd40de392bdd1d0e4e48ae4e7c67e38
H. Turgut Uyar <uyar@tekir.org>
2021-01-31 18:06:50 +0300

    switch from scss to gulp+postcss

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+
+gulp.task('css', () => {
+    const postcss = require('gulp-postcss');
+
+    return gulp.src('asset_sources/css/main.css')
+      .pipe(postcss())
+      .pipe(gulp.dest('assets/static/'));
+});
+
+gulp.task('build', gulp.series('css'));
+
+gulp.task('watch', () => {
+    gulp.watch('asset_sources/css/*.css', gulp.series('css'));
+});
+
+gulp.task('default', gulp.series('build'));
---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('postcss-discard-comments'),
+        require('stylefmt'),
+    ],
+};
